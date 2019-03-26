93b87d358eb8b65b55ca19917c9d422bb895289d
Sirikonss <47658353+Sirikonss@users.noreply.github.com>
2019-03-26 12:34:54 +0700

    Fruits

--- Fruto.py
+++ Fruto.py
@@ -1,5 +1,7 @@
 import arcade
 from Models import Player,World
+from random import randint,random
+import time
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
@@ -20,6 +22,7 @@ class ModelSprite(arcade.Sprite):
 
 
 class FrutoWindow(arcade.Window):
+    FRUITS_LIST = ['images/apple.png','images/pineapple.png','images/banana.png','images/orange.png','images/watermelon.png']
     def __init__(self, width, height):
         super().__init__(width, height)
 
@@ -27,18 +30,45 @@ class FrutoWindow(arcade.Window):
         self.background = arcade.load_texture('images/background.png')
         self.world = World(width, height)
         self.player_sprite = ModelSprite('images/girl.png', model=self.world.player)
+        self.fruits_list_sprite = []
+        self.block1_sprite = ModelSprite(FrutoWindow.FRUITS_LIST[randint(0,4)],model=self.world.block_1)
+        self.block2_sprite = ModelSprite(FrutoWindow.FRUITS_LIST[randint(0,4)],model=self.world.block_2)
+        self.block3_sprite = ModelSprite(FrutoWindow.FRUITS_LIST[randint(0,4)],model=self.world.block_3)
+        self.block4_sprite = ModelSprite(FrutoWindow.FRUITS_LIST[randint(0,4)],model=self.world.block_4)
+        self.block5_sprite = ModelSprite(FrutoWindow.FRUITS_LIST[randint(0,4)],model=self.world.block_5)
+
+
+
 
     def on_draw(self):
         arcade.start_render()
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
                                       SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+
+        self.block1_sprite.draw()
+        self.block2_sprite.draw()
+        self.block3_sprite.draw()
+        self.block4_sprite.draw()
+        self.block5_sprite.draw()
         self.player_sprite.draw()
+
+
+    def level1(self) :
+        A = self.apple_sprite.draw()
+        P = self.pine_sprite.draw()
+        fruits = [P,P,P]
+        for i in range(len(fruits)):
+            fruits[i]
+
+
+
 
     def on_key_press(self, key, key_modifiers):
         self.world.on_key_press(key, key_modifiers)
 
     def update(self, delta):
         self.world.update(delta)
+
 
 
 
--- Models.py
+++ Models.py
@@ -1,12 +1,54 @@
+import arcade.key
+from random import randint
+import sys
+
+DIR_STILL = 0
+DIR_LEFT = 1
+DIR_RIGHT = 2
+
+DIR_OFFSETS = { DIR_STILL: (0,0),
+                DIR_RIGHT: (1,0),
+                DIR_LEFT: (-1,0) }
+
+MOVEMENT_SPEED = 5
+
 class Player :
     def __init__(self,x,y) :
         self.x = x
         self.y = y
+        self.direction = DIR_STILL
+
+    def move(self,direction):
+        self.x += DIR_OFFSETS[direction][0] * MOVEMENT_SPEED
+        self.y += DIR_OFFSETS[direction][1] * MOVEMENT_SPEED
+
+    def hit(self, other, hit_size):
+        return (abs(self.x - other.x) <= hit_size) and (abs(self.y - other.y) <= hit_size)
 
     def update(self,delta):
-        if self.x > 1000:
-            self.x = 0
-        self.x += 0
+        self.move(self.direction)
+
+class Fruits :
+    def __init__(self,x1,x2) :
+        self.x = randint(x1,x2)
+        self.y = randint(600,900)
+
+
+    def has_apple_at(self, r, c):
+        return self.map[r][c] == 'A'
+
+    def has_pineapple_at(self, r, c):
+        return self.map[r][c] == 'P'
+
+    def update(self,delta,x1,x2) :
+        self.y -= 1
+        if self.y == 50 :
+            self.x = randint(x1,x2)
+            self.y = randint(600,900)
+
+
+
+
 
 
 class World :
@@ -14,9 +56,25 @@ class World :
         self.width = width
         self.height = height
         self.player = Player( 500, 150)
+        self.block_1 = Fruits(100,250)
+        self.block_2 = Fruits(250,500)
+        self.block_3 = Fruits(500,650)
+        self.block_4 = Fruits(650,800)
+        self.block_5 = Fruits(800,900)
+
+    def on_key_press(self, key, key_modifiers):
+        if key == arcade.key.LEFT:
+            self.player.direction = DIR_LEFT
+        elif key == arcade.key.RIGHT:
+            self.player.direction = DIR_RIGHT
+        elif key == arcade.key.DOWN:
+            self.player.direction = DIR_STILL
+
 
     def update(self,delta) :
         self.player.update(delta)
-
-
-
+        self.block_1.update(delta,100,250)
+        self.block_2.update(delta,250,500)
+        self.block_3.update(delta,500,650)
+        self.block_4.update(delta,650,800)
+        self.block_5.update(delta,800,900)
