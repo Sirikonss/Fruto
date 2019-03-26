45a3430d3534e3ae517915d9f931661cd5be2fb8
Sirikonss <47658353+Sirikonss@users.noreply.github.com>
2019-03-26 13:50:25 +0700

    Add files via upload

--- Models.py
+++ Models.py
@@ -22,8 +22,8 @@ class Player :
         self.x += DIR_OFFSETS[direction][0] * MOVEMENT_SPEED
         self.y += DIR_OFFSETS[direction][1] * MOVEMENT_SPEED
 
-    def hit(self, other, hit_size):
-        return (abs(self.x - other.x) <= hit_size) and (abs(self.y - other.y) <= hit_size)
+    def hit(self, other):
+        return (self.x-50 < other.x < self.x+5) and (self.y+30 < other.y < self.y+50)
 
     def update(self,delta):
         self.move(self.direction)
@@ -32,23 +32,14 @@ class Fruits :
     def __init__(self,x1,x2) :
         self.x = randint(x1,x2)
         self.y = randint(600,900)
+        self.status = True
 
-
-    def has_apple_at(self, r, c):
-        return self.map[r][c] == 'A'
-
-    def has_pineapple_at(self, r, c):
-        return self.map[r][c] == 'P'
 
     def update(self,delta,x1,x2) :
         self.y -= 1
         if self.y == 50 :
             self.x = randint(x1,x2)
             self.y = randint(600,900)
-
-
-
-
 
 
 class World :
@@ -73,8 +64,24 @@ class World :
 
     def update(self,delta) :
         self.player.update(delta)
-        self.block_1.update(delta,100,250)
-        self.block_2.update(delta,250,500)
-        self.block_3.update(delta,500,650)
-        self.block_4.update(delta,650,800)
-        self.block_5.update(delta,800,900)
+        if self.player.hit(self.block_1) is True :
+            print('hit')
+            self.block_1.update(delta,100,250)
+        elif self.player.hit(self.block_2) is True:
+            print('hit')
+            self.block_2.update(delta,250,500)
+        elif self.player.hit(self.block_3) is True:
+            print('hit')
+            self.block_3.update(delta,500,650)
+        elif self.player.hit(self.block_4) is True:
+            print('hit')
+            self.block_4.update(delta,650,800)
+        elif self.player.hit(self.block_5) is True:
+            print('hit')
+            self.block_5.update(delta,800,900)
+        else :
+            self.block_1.update(delta,100,250)
+            self.block_2.update(delta,250,500)
+            self.block_3.update(delta,500,650)
+            self.block_4.update(delta,650,800)
+            self.block_5.update(delta,800,900)
